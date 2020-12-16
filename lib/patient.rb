115c35908da3793446f1965ae2837457debe8955
Alex Marz <alexhmarz@gmail.com>
2020-12-15 22:22:02 -0500

    commit tree 45d2a249113f2e2e654c36e19137ed127e22db7c

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,29 @@
+class Patient
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select{|appt| appt.patient == self}
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+    
+
+    def doctors
+        Appointment.all.map{|appt| appt.doctor}
+    end
+end
 
